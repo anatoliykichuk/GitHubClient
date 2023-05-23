bcb96489d631d4e707453383b347a290b336792a
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-05-23 15:55:25 +0300

    Initial commit

---
+++ app/src/main/java/com/geekbrains/githubclient/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geekbrains.githubclient.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geekbrains.githubclient.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
