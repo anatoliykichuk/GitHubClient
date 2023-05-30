de392144cb4779ec36703766a575dab2de25e94c
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2023-05-30 11:59:35 +0300

    Реализованы обработчики событий для сохранения и восстановления данных данных

--- app/src/main/java/com/geekbrains/githubclient/ui/MainActivity.kt
+++ app/src/main/java/com/geekbrains/githubclient/ui/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.geekbrains.githubclient.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.geekbrains.githubclient.R
+import androidx.appcompat.app.AppCompatActivity
+import com.geekbrains.githubclient.databinding.ActivityMainBinding
+
+private const val countersKey = "COUNTERS"
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private val counters = mutableListOf<Int>(0, 0, 0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setCountersClickListener()
+        showCountersResults()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray(countersKey, counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedCounters = savedInstanceState.getIntArray(countersKey)
+        savedCounters?.toList()?.let {
+            counters.clear()
+            counters.addAll(it)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun setCountersClickListener() {
+        binding.counter1.setOnClickListener {
+            counters[0]++
+            showCountersResults()
+        }
+
+        binding.counter2.setOnClickListener {
+            counters[1]++
+            showCountersResults()
+        }
+
+        binding.counter3.setOnClickListener {
+            counters[2]++
+            showCountersResults()
+        }
+    }
+
+    private fun showCountersResults() {
+        binding.counter1.text = counters[0].toString()
+        binding.counter2.text = counters[1].toString()
+        binding.counter3.text = counters[2].toString()
     }
 }
